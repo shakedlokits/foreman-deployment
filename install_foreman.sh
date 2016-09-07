90b1bf7056d00e4aaccd167315669b30110ec656
Shaked Lokits <shaked.lokits@gmail.com>
2016-09-07 15:20:36 +0300

    changed ENV name

--- install_foreman.sh
+++ install_foreman.sh
@@ -26,16 +26,16 @@ REPO_BRANCH=1.12-stable
 
 # get latest foreman repo, configure and install dependencies
 git clone ${REPO_URL} -b ${REPO_BRANCH} /usr/src/app
-cp $REPO_LOCATION/Gemfile.local.rb bundler.d/Gemfile.local.rb
-cp $REPO_LOCATION/settings.yaml config/settings.yaml
-cp $REPO_LOCATION/database.yml config/database.yml
+cp $WORKSPACE/foreman-deployment/Gemfile.local.rb bundler.d/Gemfile.local.rb
+cp $WORKSPACE/foreman-deployment/settings.yaml config/settings.yaml
+cp $WORKSPACE/foreman-deployment/database.yml config/database.yml
 
 # uncomment when testing develop
 # [ -e "package.json" ]
 # npm install || exit 0
 
 # apply simplecov patch to foreman stable branch
-cp $REPO_LOCATION/simplecov-support.patch simplecov-support.patch
+cp $WORKSPACE/foreman-deployment/simplecov-support.patch simplecov-support.patch
 git config --global user.email "slokits@redhat.com"
 git config --global user.name "Shaked Lokits"
 git am < simplecov-support.patch
